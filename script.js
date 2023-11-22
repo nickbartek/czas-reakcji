2779f87aa80fadd7e7f8cd1f3b62bc007be536a0
tutajbartek@gmail.com <tutajbartek@gmail.com>
2023-11-22 18:16:30 +0100

    work as I wish
    Signed-off-by: tutajbartek@gmail.com <tutajbartek@gmail.com>

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
-var canClick = true; // Zmienna śledząca, czy gracz może kliknąć
+var canClick = true;
 
-function getCenterPosition() {
-  var x = window.innerWidth;
-  var y = window.innerHeight;
-  var centerX = Math.floor(x / 2 - 50); // 50 to połowa szerokości kwadratu
-  var centerY = Math.floor(y / 2 - 50); // 50 to połowa wysokości kwadratu
-  return [centerX, centerY];
+
+function getRandomPosition() {
+  var x = window.innerWidth /2;
+  var y = window.innerHeight /2;
+  var randomX = Math.floor(x);
+  var randomY = Math.floor(y);
+  return [randomX, randomY];
 }
 
 function showTarget() {
   var target = document.getElementById('target');
-  var position = getCenterPosition();
+  var position = getRandomPosition();
   target.style.left = position[0] + 'px';
   target.style.top = position[1] + 'px';
   target.style.display = 'block';
   startTime = new Date();
-  canClick = true; // Ustawia zmienną na true, aby gracz mógł kliknąć
+  canClick = true;
+    
+  randomshow= Math.random() *10
+  if(randomshow>5) {
+  // Pokazuje dystraktor
+  var distractor = document.getElementById('distractor');
+  var distractorPosition = getRandomPosition();
+  distractor.style.left = distractorPosition[0] + 'px';
+  distractor.style.top = distractorPosition[1] + 'px';
+  distractor.style.display = 'block';
+}
 }
 
-function hitTarget() {
+function hitTarget(event) {
   if (!canClick) {
-    return; // Jeśli nie można kliknąć, nie wykonuj reszty funkcji
+    return;
   }
 
-  var endTime = new Date();
-  var timeDiff = endTime - startTime;
-  alert('Twój czas reakcji wyniósł ' + timeDiff + ' milisekund.');
+  var clickedElement = event.target;
+  if (clickedElement.id === 'target') {
+    var endTime = new Date();
+    var timeDiff = endTime - startTime;
+    alert('Twój czas reakcji wyniósł ' + timeDiff + ' milisekund.');
+  } else if (clickedElement.id === 'distractor') {
+    alert('Ooops! Kliknąłeś dystraktor. Gra zrestartowana.');
+  }
+
   document.getElementById('target').style.display = 'none';
-  canClick = false; // Ustawia zmienną na false, aby gracz nie mógł kliknąć ponownie od razu
+  document.getElementById('distractor').style.display = 'none';
+  canClick = false;
   setTimeout(showTarget, Math.random() * 5000 + 2000);
 }
 
 var startTime;
-document.getElementById('target').addEventListener('click', hitTarget);
+document.addEventListener('click', hitTarget);
 setTimeout(showTarget, Math.random() * 5000 + 2000);
